cf9fcc16edbc630f1265971a1b0dd3611d908b63
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2026-02-14 12:04:28 +0100

    Update reddit_translations.py

--- reddit_translations.py
+++ reddit_translations.py
@@ -2,7 +2,7 @@ from flask_babel import gettext as _
 from searx.plugins import Plugin
 
 
-class MyPlugin(Plugin):
+class RedditTranslationFilterPlugin(Plugin):
     id = "reddit_translation_filter"
 
     def __init__(self, plg_cfg):
@@ -13,11 +13,11 @@ class MyPlugin(Plugin):
             description=_("Filters auto-translated posts from results"),
         )
 
-    def my_url_filter(result, field_name, url_src) -> bool | str:
+    def reddit_translation_filter(result, field_name, url_src) -> bool | str:
         if "reddit.com" not in url_src:
             return True
         return "?tl=" not in url_src
 
     def on_result(self, request, search, result) -> bool:
-        result.filter_urls(self.my_url_filter)
+        result.filter_urls(RedditTranslationFilterPlugin.reddit_translation_filter)
         return True
